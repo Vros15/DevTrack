e3295d4823638d829252a1f30bdcd1211f625547
Victor Rosario <vros15@yahoo.com>
2026-09-03 13:32:20 -0400

    feat(auth): add login and logout url routes

--- job_application_tracker/app/urls.py
+++ job_application_tracker/app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("signup/", views.signup, name="signup"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
 ]
