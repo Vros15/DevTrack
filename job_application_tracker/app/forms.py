820fc20194d104e90cead8542145f238d42602ae
Victor Rosario <vros15@yahoo.com>
2026-09-03 16:58:19 -0400

    feat(forms): create jobapplicationform modelform

--- job_application_tracker/app/forms.py
+++ job_application_tracker/app/forms.py
@@ -1,9 +1,12 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import JobApplication
+
 User = get_user_model()
 
-
+# Defines a form for user sign-up, extending Django's built-in UserCreationForm
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
@@ -12,4 +15,16 @@ class SignUpForm(UserCreationForm):
             "email",
             "password1",
             "password2",
+        )
+# Defines a form for creating and updating job applications
+class JobApplicationForm(forms.ModelForm):
+    class Meta:
+        model = JobApplication
+        fields = (
+            "company",
+            "position",
+            "status",
+            "job_url",
+            "notes",
+            "applied_date",
         )
