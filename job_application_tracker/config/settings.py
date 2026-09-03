348ffdb8899718d95dc3b0ca2b5302de01db4cdb
Victor Rosario <vros15@yahoo.com>
2026-09-03 11:48:41 -0400

    fix(settings): allow all hosts in allowed_hosts

--- job_application_tracker/config/settings.py
+++ job_application_tracker/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5q*yrb1v9!v-hjn1@&lfvh=%iw8gfy4^7eomo8b8kj-49n&4sd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
