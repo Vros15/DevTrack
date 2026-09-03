0ba9133791090534a095efca4ed885468338bde8
Victor Rosario <vros15@yahoo.com>
2026-09-03 12:57:22 -0400

    feat(auth): add signup view with user creation and login handling

--- job_application_tracker/app/views.py
+++ job_application_tracker/app/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+
+from .forms import SignUpForm
 
 
 def home(request):
     return render(request, "app/home.html")
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        #If the form is valid, save the user and log them in, then redirect to the home page
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+
+            return redirect("home")
+    else:
+        form = SignUpForm()
+
+    # If the form is not valid or the request method is not POST, render the signup page with the form
+    return render(
+        request,
+        "app/signup.html",
+        {
+            "form": form,
+        },
+    )
