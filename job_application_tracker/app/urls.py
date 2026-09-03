ac439b3c48f97226d80b5e2e358d20fa3b0c39ca
Victor Rosario <vros15@yahoo.com>
2026-09-03 16:58:53 -0400

    feat(urls): add application_create route

--- job_application_tracker/app/urls.py
+++ job_application_tracker/app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
     path("applications/", views.application_list, name="application_list"),
+    path("applications/new/", views.application_create, name="application_create"),
 ]
